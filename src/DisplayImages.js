7a6d57cc73df7d807455e131fca6b7297a64282b
jtoland <jtoland3.14@gmail.com>
2021-04-25 16:29:25 -0600

    Adding image objects

--- src/DisplayImages.js
+++ src/DisplayImages.js
@@ -4,14 +4,75 @@ import jop from './images/jop.jpg';
 
 const images = [
     {
-        source: jop 
+        caption: "Murderbook Wall Page",
+        alt: "Murderbook Wall Page image",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
     },
     {
-        source: jop
+        caption: "Post Comments",
+        alt: "Murderbook Post Comments view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
     },
     {
-        source: jop
-    }
+        caption: "Post Writting Comments",
+        alt: "Murderbook Post Writting Comments view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Sharing Post",
+        alt: "Murderbook Sharing Post view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Post Kabob Menu",
+        alt: "Murderbook Post Kabob Menu view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Recommended Friends",
+        alt: "Murderbook Recommended Friends view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Freind Requests",
+        alt: "Murderbook Freind Requests view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "People You May Know",
+        alt: "Murderbook People You May Know view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Story View",
+        alt: "Murderbook Story view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
+    {
+        caption: "Additional Story View",
+        alt: "Murderbook Additional Story view",
+        source: {
+            regular: "A URL to load a high quality image from"
+        }
+    },
 ];
 
 class DisplayImages extends Component {
